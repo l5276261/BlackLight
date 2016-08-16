fdc4a3922302f40aa2a96bc0af42434a64860d33
LongJue <190565682@qq.com>
2016-08-16 18:36:24 +0800

    add Assets files to github

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour {
+
+	private float _moveSpeed = 10.0f;
+	private float _endZ = -20.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.z < _endZ) {
+			transform.Translate (Vector3.forward*_moveSpeed*Time.deltaTime);
+		}
+	}
+}
---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuManager : MonoBehaviour {
+
+	public GameObject ButtonContains;
+	public GameObject Tip;
+
+	void Update()
+	{
+		if(Input.GetMouseButtonDown(0) && !ButtonContains.gameObject.activeInHierarchy)
+		{
+			ShowButton ();
+		}
+	}
+
+	public void ShowButton()
+	{
+		ButtonContains.gameObject.SetActive (true);
+		Tip.gameObject.SetActive (false);
+	}
+
+	public void ClickedNewGameButton()
+	{
+		PlayerPrefs.SetInt ("InputFromNewScene", 1);
+	}
+
+	public void ClickedLoadGameButton()
+	{
+		PlayerPrefs.SetInt ("InputFromNewScene", 0);
+	}
+}
